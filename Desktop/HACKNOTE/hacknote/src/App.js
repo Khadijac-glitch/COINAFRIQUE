6b1ccbaa618f46257513b3dee9d49eae719981d1
Khadija <dija5631@gmail.com>
2024-05-21 10:32:50 +0200

    first

---
+++ Desktop/HACKNOTE/hacknote/src/App.js
@@ -0,0 +1,12 @@
+import NoteHack from './Component/NoteHack';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <NoteHack/>
+    </div>
+  );
+}
+
+export default App;
